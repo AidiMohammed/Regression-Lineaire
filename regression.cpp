46af736dc1d7fa2c94b29c4ed7a7853b95a53af8
Mohammed Aidi <myster.oujda.48@gmail.com>
2022-07-12 23:49:50 +0100

    updating

--- regression.cpp
+++ regression.cpp
@@ -7,34 +7,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <windows.h>
 
-#pragma warning(disable : 4996)
+#pragma warning(disable : 4996) 
 
 using namespace std;
 
-void getDataFromFile(string filPath, vector<vector<float>> &features,vector<float> &targets)
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+
+void gotoxy(short a, short b) //Custom gotoxy() function
+{
+	COORD coordinates; //Data type of co-ordinates
+	coordinates.X = a; //Assign value to X- Co-ordinate
+	coordinates.Y = b; //Assign value to Y Co-ordinate
+
+	SetConsoleCursorPosition(
+		GetStdHandle(STD_OUTPUT_HANDLE), coordinates);
+
+}
+
+void testcolor()
+{
+	for (size_t i = 0; i < 255; i++)
+	{
+		SetConsoleTextAttribute(hConsole, (WORD)i);
+		cout << "TEST : " << i << endl;
+	}
+
+	system("pause");
+}
+
+bool getDataFromFile(string filPath, vector<vector<float>> &features, vector<float> &targets)
 {
 	/*
 	* this method which allows to recover the data from a file and the file must be of a format (X = feateurs Y = Targets)
-	*   +----+----+----+----+----+----+
-	*	| X1 | X2 | X3 |... | Xn |  Y |
-	*   +----+----+----+----+----+----+
-	*   |0.1 |0.5 |0.8 |... |0.5 |0.8 |
+
+	*   0.1 0.5 0.8 ... 0.5 0.8
+	*   0.7 3.2 3.8 ... 1.6 0.1
 	*                .
 	*		 	     .
 	*				 .
-	*	|0.3 |0.7 |1.8 |... |9.5 |8.8 |
+	*	0.3 0.7 1.4 ... 9.0 9.2
 	*/
 
 	FILE* file = NULL;
 	file = fopen(filPath.c_str(), "r");
 	if (!file)
+	{
 		cout << "**** Impossible d'ouvrir le fichier "<< filPath << " ****" << endl;
+		return false;
+	}
 	else
 	{
 		int currentCaracter = 0;
 		int ligne = 0;
+		float rowFile = 0;
+		int lastProgress = 0;
 		vector<float> vectorLigne;
+
+		cout << endl << endl <<"	Initialization ... " << endl;
+
+		do//get number of row from file to calculate file refing progress
+		{
+			currentCaracter = fgetc(file);
+
+			if (currentCaracter == '\n')
+				rowFile++;
+
+		} while (currentCaracter != EOF);
+
+		rewind(file);//puts the cursor back to the beginning of the file
 
 		do
 		{
@@ -52,7 +95,45 @@ void getDataFromFile(string filPath, vector<vector<float>> &features,vector<floa
 						features.at(ligne).push_back(vectorLigne.at(i));
 				}
 
+				if(ligne > 0)
+				if (vectorLigne.size() != features.at(ligne - 1).size())
+				{
+					SetConsoleTextAttribute(hConsole, 207);
+					string messageError = "    /!\\ The File " + filPath + " is corrupted, check your data file    ";
+					cout << endl;
+
+					for (size_t i = 0; i < messageError.length(); i++)
+						cout << " ";
+
+					cout << endl;
+					cout << messageError<< endl;
+
+					for (size_t i = 0; i < messageError.length(); i++)
+						cout << " ";
+
+					cout << endl;
+					cout << endl;
+					SetConsoleTextAttribute(hConsole, 15);
+
+					return false; 
+				}
+
 				ligne++;
+				float progresion = 100 + (ligne - rowFile) / rowFile * 100;
+
+				if (ligne == 1)
+				{
+					gotoxy(0, 5);
+					cout << "Loding File DATA ...";
+				}
+
+				if (lastProgress != (int)progresion)//for optimisation
+				{
+					gotoxy(21, 5);
+					cout << " " <<(int)progresion << "%" << endl;
+					lastProgress = (int)progresion;
+				}
+				
 				vectorLigne.clear();
 			}
 
@@ -63,7 +144,10 @@ void getDataFromFile(string filPath, vector<vector<float>> &features,vector<floa
 
 			if (currentCaracter == EOF)
 				vectorLigne.push_back(tempData);
+
 		} while (currentCaracter != EOF);
+
+		fclose(file);
 
 		//persist last ligne data
 		features.push_back(vector<float>(1, 1));
@@ -77,7 +161,24 @@ void getDataFromFile(string filPath, vector<vector<float>> &features,vector<floa
 				features.at(ligne).push_back(vectorLigne.at(i));
 		}
 
-		fclose(file);
+		string messageSuccsess = "    File "+filPath+" is loding    ";
+
+		cout << endl;
+
+		SetConsoleTextAttribute(hConsole, 47);
+		for (size_t i = 0; i < messageSuccsess.length(); i++)
+			cout << " ";
+		cout << endl;
+
+		cout << messageSuccsess << endl;
+
+		for (size_t i = 0; i < messageSuccsess.length(); i++)
+			cout << " ";
+		cout << endl << endl;
+		SetConsoleTextAttribute(hConsole, 15);
+
+
+		return true;
 	}
 }
 
@@ -86,7 +187,7 @@ void showFeatures(vector<vector<float>> features)
 	for (size_t i = 0; i < features.size(); i++)
 	{
 		for (size_t y = 0; y < features.at(i).size(); y++)
-			printf("[%f]", features.at(i).at(y));
+			cout << "[" << features.at(i).at(y) << "]";
 		cout << endl;
 	}
 }
@@ -94,23 +195,123 @@ void showFeatures(vector<vector<float>> features)
 void showTargets(vector<float> targets)
 {
 	for (size_t i = 0; i < targets.size(); i++)
-		printf("[%f] \n", targets.at(i));
+		cout << "[" << targets.at(i) << "]" << endl;
+}
+
+float linearModel(const vector<float> inputs,vector<float> &thetas)
+{
+	float Result = 0;
+
+	if (thetas.empty())
+		for (size_t i = 0; i < inputs.size(); i++)
+			thetas.push_back(0.0);
+
+	for (size_t i = 0; i < inputs.size(); i++)
+		Result += (thetas.at(i) * inputs.at(i));
+
+	return Result;
+}
+
+float ploynomialeMode(const vector<float> inpusts, vector<float> &thetas)
+{
+
+}
+
+float costFunction(const vector<vector<float>> features, const vector<float> targets, vector<float> &thetas)
+{
+	float sumDeltaSquared = 0.0;
+	float meanSquaredError = 0;
+
+	for (size_t row = 0; row < features.size(); row++)
+		sumDeltaSquared += pow(linearModel(features.at(row),thetas) - targets.at(row),2);
+
+	meanSquaredError = (features.size() * 2) / 1 * sumDeltaSquared;
+	return meanSquaredError;
+}
+
+vector<float> gradientDescent(const vector<vector<float>> features,const vector<float> targts,vector<float> &thetas)
+{
+	double alpha = 0.1;
+	vector<float> derivatives;
+	float result = 0.0;
+
+	for (size_t i = 0; i < thetas.size(); i++)
+	{
+		for (size_t row = 0; row < features.size(); row++)
+			result += (linearModel(features.at(row), thetas) - targts.at(row)) * features.at(row).at(i);
+
+		derivatives.push_back(result);
+	}
+
+	return derivatives;
 }
 
 int main()
 {
-	float alpha = 0.000001;
-
 	vector<vector<float>> features(0,vector<float> (0));
 	vector<float> targets;
 	vector<float> thetas;
 	vector<float> thetasUpdate;
-	vector<float> prediction;
 
-	getDataFromFile("data-test.txt",features,targets);
+	int lastProsessing = 0;
+	FILE* file = NULL;
+
+	//testcolor();
+
+	if (getDataFromFile("DataLR_HighNextBar.data", features, targets))
+	{
+		float iterations = 300;
+
+		for (size_t i = 0; i < (size_t)iterations; i++)
+		{
+			file = fopen("SaveCosts.txt", "a+");
+			if (!file)
+			{
+				cout << "**** Impossible d'ouvrir le fichier SaveCosts.txt ****" << endl;
+				return false;
+			}
+
+			thetasUpdate = gradientDescent(features, targets, thetas);
+
+			for (size_t i = 0; i < thetas.size(); i++) 
+				thetas.at(i) = thetas.at(i) - 0.0000001 * thetasUpdate.at(i);
+
+			float progresion = 100 + (i+1 - iterations) / iterations * 100;
+
+			if (i == 0)
+			{
+				gotoxy(0, 11);
+				cout << "Processing ... 0%";
+			}
+
+			if (lastProsessing != (int)progresion)
+			{
+				gotoxy(15, 11);
+				cout << " " << (int)progresion << "%" << endl;
+				lastProsessing = progresion;
+			}
+
+			costFunction(features, targets, thetas);
+
+			if (file)
+				fprintf(file, "%ld %f\n", i, costFunction(features, targets, thetas));
+			
+			fclose(file);
+		}
+
+
+		cout << "LAST Cost sunction : " << costFunction(features, targets, thetas);
+
+		/*for (size_t i = 0; i < thetas.size(); i++)
+			cout << "[" << thetas.at(i) << "] - ";*/
+	}
 }
 
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
+
+
+
+
+// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage 
 // Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
 
 // Conseils pour bien démarrer : 
